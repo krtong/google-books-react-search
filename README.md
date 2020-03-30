0dd9294e1d9b0e2b9a065e8cd806dce6cd2f393b
krtong <kevin.r.tong@gmail.com>
2020-03-29 17:45:25 -0700

    added mern stack

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Reading List - Context Demo
+
+## Instructions
+
+1) Run `npm install` to load dependencies from the root folder.
+2) Run `npm run seed` to load database with seed data.
+3) Run `npm start` to run application
+
+Like button increments likes for books.  Total likes are displayed in top nav bar.  Likes for each book are displayed in the book detail page.
---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BookContext } from './context/BookContext';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  return (
+    <BookContext.Provider value={{ books, setBooks }}>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+    </BookContext.Provider>
+  );
+}
+
+export default App;
